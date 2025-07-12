c588065738652601906791796b61ea202b4c44b4
Batuhan Sarıaslan <batu.gs262@gmail.com>
2025-07-12 14:49:04 +0300

    Delete SQL.sql

--- SQL.sql
+++
@@ -1,5 +0,0 @@
-SELECT stay AS stay, COUNT(*) AS count_int, ROUND(AVG(todep),2) AS average_phq, ROUND(AVG(tosc),2) AS average_scs, ROUND(AVG(toas),2) AS average_as
-FROM students
-WHERE inter_dom = 'Inter'
-GROUP BY stay
-ORDER BY stay DESC;
